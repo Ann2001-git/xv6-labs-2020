56ff2c845316a1185bb150447ec034842073d1f4
Liu Miao <liumiaogemini@foxmail.com>
2021-07-23 00:09:24 +0800

    Lab pgtbl: vmprint

--- kernel/vm.c
+++ kernel/vm.c
@@ -440,3 +440,23 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+static void
+pteprint(pagetable_t pagetable, int depth){
+  for(int i = 0; i < 512; i++){
+    pte_t pte = pagetable[i];
+    if(pte & PTE_V){
+      uint64 child = PTE2PA(pte);
+      for(int j = 0; j < depth; j++)
+        printf(" ..");
+      printf("%d: pte %p pa %p\n", i, pte, child);
+      if((pte & (PTE_R | PTE_W | PTE_X)) == 0)
+        pteprint((pagetable_t)child,depth+1);
+    }
+  }
+}
+
+void vmprint(pagetable_t pagetable){
+  printf("page table %p\n", pagetable);
+  pteprint(pagetable, 1);
+}
