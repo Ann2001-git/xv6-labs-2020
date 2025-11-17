facc01e08aec779311b56e6c207b61b1bda65d24
Ann2001-git <AR99413429@163.com>
2025-11-17 21:45:59 +0800

    finish lab3.1

--- kernel/defs.h
+++ kernel/defs.h
@@ -183,7 +183,7 @@ uint64          walkaddr(pagetable_t, uint64);
 int             copyout(pagetable_t, uint64, char *, uint64);
 int             copyin(pagetable_t, char *, uint64, uint64);
 int             copyinstr(pagetable_t, char *, uint64, uint64);
-void            vmprint(pagetable_t);
+void            vmprint(pagetable_t,int);
 
 // vmcopyin.c
 int copyin_new(pagetable_t, char*, uint64, uint64);
--- kernel/vm.c
+++ kernel/vm.c
@@ -499,22 +499,35 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
 #endif
 }
 
-static void
-pteprint(pagetable_t pagetable, int depth){
+// static void
+// pteprint(pagetable_t pagetable, int depth){
+//   for(int i = 0; i < 512; i++){
+//     pte_t pte = pagetable[i];
+//     if(pte & PTE_V){
+//       uint64 child = PTE2PA(pte);
+//       for(int j = 0; j < depth; j++)
+//         printf(" ..");
+//       printf("%d: pte %p pa %p\n", i, pte, child);
+//       if((pte & (PTE_R | PTE_W | PTE_X)) == 0)
+//         pteprint((pagetable_t)child,depth+1);
+//     }
+//   }
+// }
+
+void vmprint(pagetable_t pagetable,int depth){
+ if(depth == 0) printf("page table %p",pagetable);
+ if(depth>2) return;
   for(int i = 0; i < 512; i++){
     pte_t pte = pagetable[i];
-    if(pte & PTE_V){
-      uint64 child = PTE2PA(pte);
-      for(int j = 0; j < depth; j++)
+    if((pte & PTE_V) != 0){ 
+      printf(" ..");
+      for(int j = 0;j<depth;j++){
         printf(" ..");
-      printf("%d: pte %p pa %p\n", i, pte, child);
-      if((pte & (PTE_R | PTE_W | PTE_X)) == 0)
-        pteprint((pagetable_t)child,depth+1);
+      }
+      uint64 child = PTE2PA(pte);
+      printf("%d: pte %p pa %p\n",i,pte,child);
+       
+      vmprint((pagetable_t)child,depth+1); 
     }
   }
-}
-
-void vmprint(pagetable_t pagetable){
-  printf("page table %p\n", pagetable);
-  pteprint(pagetable, 1);
 }
